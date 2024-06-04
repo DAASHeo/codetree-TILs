4968e28bfa75364af4fa568952e4b8071edd0935
Heo Eun <64454313+DAASHeo@users.noreply.github.com>
2024-06-04 19:10:38 +0900

    Title: 격자에 점 그리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240604/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+point = 1
+place = []
+array = [[0] * n for i in range(5)]
+
+for i in range(m):
+    place.append(list(map(int, input().split())))
+    place[i].append(point)
+    point += 1
+
+for r, c, p in place:
+    array[r-1][c-1] = p
+
+for i in range(n):
+    for j in range(n):
+        print(array[i][j], end=" ")
+    print()
